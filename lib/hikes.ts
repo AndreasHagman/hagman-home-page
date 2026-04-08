fc404f109e7c3b2b94764686a6d9c41cd30e3336
Andreas Hagman <andreas.jorgensrud.hagman@storebrand.no>
2026-04-08 21:28:41 +0200

    add skjerag

--- lib/hikes.ts
+++ lib/hikes.ts
@@ -20,6 +20,12 @@ export const hikes: Hike[] = [
     description: 'The famous cliff rising 604 metres above Lysefjord. Stunning panoramic views and a rewarding trail.',
   },
   {
+    name: 'Kjeragbolten',
+    location: 'Rogaland, Norway',
+    year: 2023,
+    description: 'A boulder wedged in a crevice 984 metres above Lysefjord. One of the most iconic photo spots in Norway.',
+  },
+  {
     name: 'Galdhøpiggen',
     location: 'Jotunheimen, Norway',
     year: 2021,
